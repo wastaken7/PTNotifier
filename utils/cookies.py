242fc82559f88e076fbef364d1b1d9e1c28abab6
wastaken7 <46319635+wastaken7@users.noreply.github.com>
2026-02-14 13:38:46 -0300

    response error print

--- utils/cookies.py
+++ utils/cookies.py
@@ -17,7 +17,7 @@ def valid_response(tracker: str, response: str, keyword: str) -> bool:
     """
     if keyword and keyword not in response:
         log.error(
-            f"{tracker}: [bold red]Request failed. The cookie appears to be expired/invalid, or the site is currently down or the HTML structure has changed. Please log in through your usual browser and export the cookies again. Keyword '{keyword}' not found.[/bold red]"
+            f"{tracker}: [bold red]Request failed.\nPossible reasons: Expired cookies, IP ban, site maintenance, HTML change.\nKeyword '{keyword}' not found in the HTML response.[/bold red]"
         )
         save_html(tracker, response)
         return False
